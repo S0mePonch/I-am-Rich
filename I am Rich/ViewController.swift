705ca0c95caa1469e0c883c395832000959ce32a
Alfonso Cano Vargas <yveltal02341@outlook.com>
2023-01-11 20:18:25 -0800

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Alfonso on 21/10/1401 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
